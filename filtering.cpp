4a3c349cf8a4fa9356dab2d5c9793b440ccd8d24
JaeHuiBoo <yeppyboo@naver.com>
2022-10-03 11:42:05 -0400

    filtering ing

--- filtering.cpp
+++ filtering.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cmath>
 #include "AudioFile.h"
+#include <ctime>
+#include <list>
+#include <functional>
 
 using namespace std;
 
@@ -8,6 +11,7 @@ using namespace std;
  namespace examples
  {
     void loadAudioFiltering();
+    void writingFile(list<float> valueList, int sampleRate, double lengthTime, int count);
  }
 
 //=======================================================================
@@ -33,52 +37,89 @@ int main()
 namespace examples
 {
     void loadAudioFiltering(){
-        //1. set a file path to an audio file on your machine
-        const string filePath = "audiosample/coyote.wav";
+        //1. Set a file path to an audio file on your machine 
+        const string filePath = "audiosample/sine-wave.wav";
 
-        //2. create an AudioFile object and load the audio file
-        AudioFile<float> a;
-        bool loadedOK = a.load (filePath);
+        //2. Create An AudioFile Object and load the Audio file
+        AudioFile<float> origin;
+        bool loadedOK = origin.load(filePath);
 
-        /*
-        If you hit this assert, then the file path above
-        probably doesn't refer to a valid audio file*/
-        assert (loadedOK);
+        //if you hit this assert then the file path above probably doesn't refer to a valie audio file
+        assert(loadedOK);
 
-        //3. Let's print out some key details
-        cout << "|=========================================================|" << endl;
-        cout << "Bit Depth: " << a.getBitDepth() << endl;
-        cout << "Length in Seconds: " << a.getLengthInSeconds() << endl;
-        cout << "Num Channels: " << a.getNumChannels() << endl;
-        cout << "Num Samples per Channel: " << a.getNumSamplesPerChannel() << endl;
-        cout << "Sample Rate: " << a.getSampleRate() << endl;
-        cout << "|=========================================================|" << endl;
-        cout << "print summary" << endl;
-        a.printSummary();
-        cout << endl;
-
-        float maxfreq = 0.0f;
-        float maxvalue = 0.0f;
-        for(int i = 0; i < a.getNumSamplesPerChannel(); i++)
-        {
-            for(int channel=0; channel < a.getNumChannels(); channel++)
-            {
-                float freq = 0.0f;
-                freq = (asin(a.samples[channel][i])*a.getSampleRate())/(2.f*M_PI*static_cast<float> (i));
-                //a.samples[channel][i] = sin ((static_cast<float> (i) / sampleRate) * frequencyInHz * 2.f * M_PI); 를 역으로 풀어서 사용.
-                if(abs(freq)>maxfreq)
-                    maxfreq=freq;
-                /* freq가 범위내에 있으면 출력하도록. 범위는 엑셀파일 참고해서 짤 것. 
-                    if(abs(freq)>_____)
-                        ~~~
-                */
-            }
-        }
-        cout << "maxfreq: " << maxfreq << endl;
-        cout << "|=========================================================|" << endl;
+        //3. Print some details
+        origin.printSummary();
         
-        //4. Write audio file to disk
-        string outputFilePath = "audiosample/writingFile.wav";
-        a.save(outputFilePath, AudioFileFormat::Aiff);
+        //4. Function 
+        /*
+            1) origin에서 freq추출하면서, freq가 범위에 들어가는지 확인
+            2) freq가 범위에 속하면, extractList에 해당 값들 추출 & 추출하기 시작했다는 신호 보내기 
+            3) freq가 범위를 벗어나는 순간, 새로운 AudioFIle<float> 에 값들 저장하여 wav파일로 추출 
+        */
+       
+        float maxfreq = 0.0f;
+        bool recording = false;
+        clock_t startTime, finishTime;
+        double lengthTime;
+        list<float> recordList;
+        int count = 0;
+        for(int i = 0; i<origin.getNumSamplesPerChannel();i++)
+        {
+                for(int channel=0; channel < origin.getNumChannels(); channel++){
+                    float freq = 0.0f;
+                    freq = (asin(origin.samples[channel][i])*origin.getSampleRate())/(2.f*M_PI*static_cast<float> (i));
+                    //origin.samples[channel][i] = sin ((static_cast<float> (i) / sampleRate) * frequencyInHz * 2.f * M_PI); 를 역으로 풀어서 사용.
+                    if(abs(freq)>maxfreq)
+                    {
+                        maxfreq = freq;
+                        cout << "MAX_FREQ : " << maxfreq << endl;
+                    }
+                    //cout << "freq : " << freq << endl;
+                    
+                    if(abs(freq)>1 && !recording) //시작할 때,
+                    {
+                        recording = true;
+                        startTime = clock();
+                        recordList.clear();
+                    }
+                    if(recording) 
+                    {
+                        if(abs(freq)>1)//도중 - 값 넣기 
+                        {
+                            cout << "recording ... freq: " << abs(freq) << endl;
+                            recordList.push_back(origin.samples[0][i]);
+                        }
+                        else{ //끝 - wav파일 추출 메소드 돌리기 
+                            recording = false;
+                            finishTime = clock();
+                            lengthTime = (double) (finishTime-startTime)/CLOCKS_PER_SEC;
+                            cout << "finish. length time : " << (double)lengthTime << endl;
+                            examples::writingFile(recordList, origin.getSampleRate(), lengthTime, count);
+                            recordList.clear();
+                            count++;
+                        }
+                    }
+                    
+                }
+        }
+    }
+    
+    void writingFile(list<float> valueList, int sampleRate, double lengthTime, int count){
+        cout << "|========== Writing File ==========|" << endl;
+        list<float>::iterator iter;
+        AudioFile<float> filteredAudio;
+        filteredAudio.setNumChannels(2);
+        filteredAudio.setNumSamplesPerChannel(sampleRate*lengthTime);
+        int i =0;
+        for(iter = valueList.begin(); iter!=valueList.end();iter++)
+        {
+            //cout << "value of iter: " << *iter << endl;
+            filteredAudio.samples[0][i] = *iter;
+            filteredAudio.samples[1][i] = *iter;
+            i++;
+        }
+        string newFilePath = "audiosample/extract_"+ to_string(count) +".wav";
+        filteredAudio.save(newFilePath, AudioFileFormat::Wave);
+        cout << "|==================================|"<< endl;
     }
 }
